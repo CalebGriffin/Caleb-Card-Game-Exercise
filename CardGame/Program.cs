8174861257358f0eb583e8676d62a68cf5fe226f
Caleb Griffin <gri18006287@students.solihull.ac.uk>
2023-11-23 16:47:15 +0000

    Created Console App Project and Skeleton Program Class

---
+++ CardGame/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CardGame
+{
+    class Program
+    {
+        private string _userInput;
+        private string[] _splitUserInput;
+        private int _score;
+
+        static void Main(string[] args)
+        {
+        }
+
+        private bool VerifyInputFormat()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool VerifyValidCard(string card)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool CheckForDuplicates()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void CalculateScore()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
